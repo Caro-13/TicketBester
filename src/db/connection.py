ea450ce50f21b268e8d1f60b84ccf68288e077ba
Caro-13 <carolina71800@gmail.com>
2026-01-10 10:52:54 +0100

    correct db timezone to CH

--- src/db/connection.py
+++ src/db/connection.py
@@ -8,10 +8,17 @@ load_dotenv()
 
 # Accessible uniquement depuis requests.py
 def _get_connection():
-    return psycopg2.connect(
+    connection =  psycopg2.connect(
         database=os.getenv("DB_NAME"),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
         host=os.getenv("DB_HOST"),
         port=os.getenv("DB_PORT")
     )
+
+    # Set timezone to Swiss time for all operations
+    cursor = connection.cursor()
+    cursor.execute("SET timezone = 'Europe/Zurich';")
+    cursor.close()
+
+    return connection
